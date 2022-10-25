94f541d6b064fa1a8ab708ab5b9f404248d47e35
rayanbelhamra <112488105+rayanbelhamra@users.noreply.github.com>
2022-10-25 09:23:32 -0400

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -5,14 +5,24 @@
 
 import random
 
-x = random.randint(0,100)
-nb_essai = 0
-print("j'ai choisi un nombre entre 0 et 100 ")
-guess = -1
-while (guess != x):
-    guess=int(input("entrez votre essai"))
-    nb_essai += 1
-    if guess < x:
-        print("votre chiffre est plus petit que celui qu'on cherche ")
-    elif guess > x:
-        print("votre chiffre est plus grand que celui qu'on cherche")
+redo = "yes"
+
+# boucle
+while redo == ("yes"):
+
+    x = random.randint(0, 100)
+    nb_essai = 0
+    print("j'ai choisi un nombre entre 0 et 100, et c'est a vous de le deviner ")
+    guess = -1
+    while (guess != x):
+        guess = int(input("entrez votre guess"))
+        nb_essai += 1
+        # ce qu'on veut que ca print si le nombre
+        if guess < x:
+            print("votre chiffre est plus petit que celui qu'on cherche ")
+        elif guess > x:
+            print("votre chiffre est plus grand que celui qu'on cherche")
+        elif guess == x:
+            print("Vous avez trouver le nombre demandé en " + str(nb_essai) "essais")
+    redo = input("Voulez vous quitter?   *repondez par yes ou par no")
+print(" thx, bye")
