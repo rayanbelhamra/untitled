8e9a0950c1247f78b33dbead09cb7dcfd51bad58
rayanbelhamra <112488105+rayanbelhamra@users.noreply.github.com>
2022-10-25 20:35:30 -0400

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -14,14 +14,18 @@ while redo == ("yes"):
     nb_essai = 0
     print("j'ai choisi un nombre entre 0 et 100, et c'est a vous de le deviner ")
     guess = -1
+    # boucle dans la boucle
     while (guess != x):
         guess = int(input("entrez votre guess"))
         nb_essai += 1
-        # ce qu'on veut que ca print si le nombre
+        # ce qu'on veut que ca print si le nombre tapé ...
+        #... est supérieur
         if guess < x:
             print("votre chiffre est plus petit que celui qu'on cherche ")
+        #... est inferieur
         elif guess > x:
             print("votre chiffre est plus grand que celui qu'on cherche")
+        #... est égale à celui qu'on cherche
         elif guess == x:
             print("Vous avez trouver le nombre demandé en " + str(nb_essai) "essais")
     redo = input("Voulez vous quitter?   *repondez par yes ou par no")
